7771aec2c48a7a6e8647b86f1c36fd44a4c41d90
julekb <julek.baranski@gmail.com>
2016-04-07 13:07:11 +0200

    instruction added

--- experiment.js
+++ experiment.js
@@ -2,6 +2,7 @@ const N = 1;
 const TIME_RESP = 10000;
 const STIMULI = ['bardzomalo', 'malo', 'srednio', 'duzo', 'bardzoduzo','mnostwo'];
 const ID;
+const entry_questions = ['Podaj adres e-mail', 'Podaj wiek'];
 
 var trial = {
 	type: 'text',
@@ -9,70 +10,33 @@ var trial = {
 };
 
 var instruction = {
-	type: 'text',
-	text: ' raz dwa </p> czy cztery' +
-	'pięć sześć </p> witaj w ekeperymencie',
-	timing_post_trial: 20
+	type: 'instructions',
+	pages: ['Witaj w eksperymencie. Nacisnij dalej',
+	'Udział w eksperymencie jest dowolny, a wyniki będą analizowane anonimowo.'],
+	show_clickable_nav: true
 };
 
-
-
-var block = [
-	{
-		stimulus: 'img/blue.png',
-		data: { response: 'b' },
-		choices: ['b']
-	},
-	{
-		stimulus: 'img/orange.png',
-		data: { response: 'o' },
-		choices: ['o']
-	}];
-var multiple_stim = jsPsych.randomization.repeat(block, N);
-var multiple_block = {
-	type: 'single-stim',
-	//choices: ['o', 'b'],
-	timing_response: TIME_RESP,
-	on_finish: function(data){
-		var correct = false;
-		if (data.response == 'b' && data.rt == -1){
-			correct = true;
-		}
-		jsPsych.data.addDataToLastTrial({correct: correct});
-	},
-	timeline: multiple_stim
-};
-
-function getSubjectData() {
-	var trials = jsPsych.data.getTrialsOfType('single-stim');
-	var sum_rt = 0;
-	var correct_trial_count = 0;
-	var correct_rt_count = 0;
-	for (var i=0; i<trials.length; i++) {
-		if (trials[i].correct == true) {
-			correct_trial_count++;
-			if (trials[i].rt > -1) {
-				sum_rt += trials[i].rt;
-				correct_rt_count++;
-			}
-		}
-	};
-	return {
-		rt: Math.floor(sum_rt/correct_rt_count),
-		accuracy: Math.floor(correct_trial_count/trials.length*100)
-	}
-}
-var debrief_block = {
-	type: 'text',
-	text: function() {
-		var subject_data = getSubjectData();
-		return "poprawność odpowiedzi: "+subject_data.accuracy+
-		"</p>średni czas odpowiedzi: "+subject_data.rt+"</p></p>"+
-		jsPsych.data.getTrialsOfType('single-stim');
-	}
-};
 
 // ################
+// TEST
+var scale = [];
+for (var i=0; i<101; i++) scale.push(i);
+
+
+var slider = {
+	type: 'survey-likert',
+	questions: ['odp1', 'odp2'],
+	labels: [scale,[]],
+
+};
+var text_response = {
+	type: 'survey-text',
+	questions: entry_questions
+};
+
+// #############
+
+// #############
 var presentation_block2 = {
 	type: 'single-stim',
 	timing_response: TIME_RESP,
@@ -99,14 +63,14 @@ var learning_loop = {
 	timeline: [presentation_block],
 	loop_function: function(){
 		//if (learning == true): return false
-		return true; 
+		return false; 
 	}
 };
 
 var timeline = [trial];
-//timeline.push(instruction);
-//timeline.push(multiple_block);
-//timeline.push(debrief_block);
+timeline.push(instruction);
+timeline.push(text_response);
+timeline.push(slider);
 timeline.push(learning_loop);
 
 jsPsych.init({
