b48b26b867c58b9f290504e5a97aac4603fee5d2
julekb <julek.baranski@gmail.com>
2016-05-27 00:17:25 +0200

    KNN dodane

--- validation.py
+++ validation.py
@@ -11,12 +11,19 @@ def CrossValidation(X, Y):
 	from sklearn.metrics import mean_squared_error as mse
 	from sklearn import cross_validation, linear_model
 	from math import exp, log
+	from sklearn.neighbors import NearestNeighbors as NN
+	from sklearn.neighbors import KNeighborsClassifier as KNC
+
 	N = 30
 
 	resultsX = []
 	resultsId = []
 	resultsRegr = []
 	resultsRegr_ey = []
+	results1NN, results2NN, results3NN = [], [], []
+
+	
+
 
 	kf = cross_validation.KFold(N, n_folds=10, random_state=True)
 	for train_index, test_index in kf:
@@ -33,10 +40,21 @@ def CrossValidation(X, Y):
 
 		regr_ey = linear_model.LinearRegression().fit(X_train_list, [exp(y) for y in Y_train])
 		resultsRegr_ey += [[log(float(regr_ey.predict(x))) for x in X_test]]
+		
+		Y_train = np.asarray(Y_train, dtype="|S6")
+		nb1NN =KNC(n_neighbors=1, algorithm='ball_tree').fit(X_train_list, Y_train)
+		results1NN += [[float(nb1NN.predict(x)[0]) for x in X_test]]
+
+		nb2NN =KNC(n_neighbors=2, algorithm='ball_tree').fit(X_train_list, Y_train)
+		results2NN += [[float(nb2NN.predict(x)[0]) for x in X_test]]
+
+		nb3NN =KNC(n_neighbors=3, algorithm='ball_tree').fit(X_train_list, Y_train)
+		results3NN += [[float(nb3NN.predict(x)[0]) for x in X_test]]
+
+	
 
 
-
-	return mse(resultsX, resultsId), mse(resultsX, resultsRegr), mse(resultsX, resultsRegr_ey)
+	return mse(resultsX, resultsId), mse(resultsX, resultsRegr), mse(resultsX, resultsRegr_ey), mse(resultsX, results1NN), mse(resultsX, results2NN), mse(resultsX, results3NN)
 
 
 
