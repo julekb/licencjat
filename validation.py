a026b659128b48822ebfdb631f4cf0cf554a285d
julekb <julek.baranski@gmail.com>
2016-05-26 21:04:47 +0200

    reg e^y

--- validation.py
+++ validation.py
@@ -1,3 +1,6 @@
+#python3
+
+
 import numpy as np
 from sklearn import cross_validation
 
@@ -7,13 +10,15 @@ def CrossValidation(X, Y):
 
 	from sklearn.metrics import mean_squared_error as mse
 	from sklearn import cross_validation, linear_model
+	from math import exp, log
 	N = 30
 
 	resultsX = []
 	resultsId = []
 	resultsRegr = []
+	resultsRegr_ey = []
 
-	kf = cross_validation.KFold(N, n_folds=10, random_state=None)
+	kf = cross_validation.KFold(N, n_folds=10, random_state=True)
 	for train_index, test_index in kf:
 		X_train, Y_train = X[train_index], Y[train_index]
 		X_test, Y_test = X[test_index], Y[test_index]
@@ -24,11 +29,14 @@ def CrossValidation(X, Y):
 		X_train_list = [[x] for x in X_train]
 
 		regr = linear_model.LinearRegression().fit(X_train_list, Y_train)
-		resultsRegr += [[int(regr.predict(x)) for x in X_test]]
+		resultsRegr += [[float(regr.predict(x)) for x in X_test]]
+
+		regr_ey = linear_model.LinearRegression().fit(X_train_list, [exp(y) for y in Y_train])
+		resultsRegr_ey += [[log(float(regr_ey.predict(x))) for x in X_test]]
 
 
 
-	return mse(resultsX, resultsId), mse(resultsX, resultsRegr)
+	return mse(resultsX, resultsId), mse(resultsX, resultsRegr), mse(resultsX, resultsRegr_ey)
 
 
 
