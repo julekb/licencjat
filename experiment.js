33222abdece101841e7135196b8e35baa2ddba6d
julekb <julek.baranski@gmail.com>
2016-11-18 19:21:55 +0100

    update

--- experiment.js
+++ experiment.js
@@ -49,6 +49,11 @@ var entry_instruction = {
 	'Jeśli zgadza sie Pani/Pan na wzięcie udziału w badaniu, proszę nacisnąć przycisk DALEJ. W przeciwnym wypadku proszę zamknąć stronę.'],
 	show_clickable_nav: true,
 };
+var sorry = {
+	type: 'instructions',
+	pages: ['Niestety, badanie zostało już zakończone.'],
+	show_clickable_nav: false,
+};
 
 var final_instruction = {
 	type: 'instructions',
@@ -90,11 +95,11 @@ var choice_response = {
 };
 
 
-var timeline = [];
-timeline.push(entry_instruction);
-timeline.push(text_response);
-timeline.push(choice_response);
-timeline.push(instruction);
+var timeline = [sorry];
+// timeline.push(entry_instruction);
+// timeline.push(text_response);
+// timeline.push(choice_response);
+// timeline.push(instruction);
 
 //adding learnig stimuli
 var training_stim = [];
@@ -105,37 +110,37 @@ for (i = 0; i < TRAINING.length; i++) {
 
 // adding learning phase
 
-for ( i = 0; i < training_stim.length; i++) {
-	var similarity_b = {
-		type: 'similarity',
-		stimuli: training_stim[i],
-		//prompt: "Suwak:",
-		show_response: "POST_STIMULUS",
-		labels: ['7', '200'],
-		img_labels: ['img/dots/dots_007.png', 'img/dots/dots_200.png'], //img labels
+// for ( i = 0; i < training_stim.length; i++) {
+// 	var similarity_b = {
+// 		type: 'similarity',
+// 		stimuli: training_stim[i],
+// 		//prompt: "Suwak:",
+// 		show_response: "POST_STIMULUS",
+// 		labels: ['7', '200'],
+// 		img_labels: ['img/dots/dots_007.png', 'img/dots/dots_200.png'], //img labels
 
-		//2 faza client.send_message(peer_id, "RESPONSE", $("#text_input").val());
+// 		//2 faza client.send_message(peer_id, "RESPONSE", $("#text_input").val());
         
-		timing_first_stim: TIME_STIM,
-		timing_image_gap: 100,
-	};
-	timeline.push(similarity_b);
-};
-timeline.push(additional_text_response);
+// 		timing_first_stim: TIME_STIM,
+// 		timing_image_gap: 100,
+// 	};
+	// timeline.push(similarity_b);
+// };
+// timeline.push(additional_text_response);
 
-var thankyou = {
-	type: 'text',
-	text: ['Wyniki zostały przesłane. Dziękuję.']
-};
+// var thankyou = {
+// 	type: 'text',
+// 	text: ['Wyniki zostały przesłane. Dziękuję.']
+// };
 
 
 jsPsych.init({
 	timeline: timeline,
-	on_finish: function(data) { 
-		saveData_csv("badanie1_", jsPsych.data.dataAsCSV());
-		jsPsych.init({
-			timeline: [thankyou]});
-	}
+// 	on_finish: function(data) { 
+// 		saveData_csv("badanie1_", jsPsych.data.dataAsCSV());
+// 		jsPsych.init({
+// 			timeline: [thankyou]});
+// 	}
 });
 
 
