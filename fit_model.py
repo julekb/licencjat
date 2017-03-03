d1d7fb5c11e1c483f1e0e37cd213fd60d60970cf
julekb <julek.baranski@gmail.com>
2017-03-03 19:17:19 +0100

    update

--- fit_model.py
+++ fit_model.py
@@ -16,8 +16,8 @@ path = "data_all/"
 with open(path+'avg_mean_sd.pkl', 'rb') as f:
 	avg_data = pkl.load(f)
 
-# with open(path+"data_all.pkl", 'rb') as f:
-with open(path+'pilot_data.pkl', 'rb') as f: #testowo mniejszy plik
+with open(path+"data_all.pkl", 'rb') as f:
+# with open(path+'pilot_data.pkl', 'rb') as f: #testowo mniejszy plik
 	data_all = pkl.load(f)
 
 def fit_model(X, Y):
@@ -31,17 +31,20 @@ def fit_model(X, Y):
 	nb3NN = KNR(n_neighbors=3, algorithm='ball_tree').fit(X, Y)
 
 	return regr, regr_ey, regr_log, nb1NN, nb2NN, nb3NN
+
 def fight(A_y, B_y, model_obj=False, model_obj_inv=False, A_model=False, A_model_inv=False, B_model=False, B_model_inv=False, k=0):
-	"""funkcja zwracająca różnicę odpowiedzi bez lub z indywidualnymi modelami
+	"""funkcja zwracająca różnicę odpowiedzi bez lub z indywidualnymi modelami oddzielnie dla uczestnika A i B
 	A_y, B_y - odpowiedzi agentów A i B
 	model_obj - model 'odniesienia/obiektywny', A_model, B_model - podele dopasowane do agentów A i B
 	jeśli modele indywidualne modele to NN, to k >= 3"""
 	if A_model == False:
-		return float(A_y - B_y)
+		return A_y - B_y, B_y - A_y
 	else:
 		d_A = A_y - float(A_model.predict(float(B_model_inv.predict(B_y))))
-		# d_B = B_model.predict(A_model_inv.predict(A_y)) - B_y
-		return d_A		
+		d_B = float(B_model.predict(float(A_model_inv.predict(A_y)))) - B_y
+		return d_A, d_B
+
+
 # sortowanie po bodźcu
 avg_data = avg_data.sort_values('stimulus')
 avg_data = avg_data.reset_index(drop=True)
@@ -60,6 +63,10 @@ X = avg_data['mean']
 
 err = []
 err_mod = [[] for i in range(6)]
+d_As = []
+d_Bs = []
+
+
 model_names = ["regr", "regr_ey", "regr_log", "nb1NN", "nb2NN", "nb3NN"]
 #parowanie każdy z każdym, dopasowanie indywidualnego modelu i symulacja
 
@@ -85,9 +92,14 @@ for A_data, B_data in combinations(data_all, 2):
 		err.append(fight(A_Y_test, B_Y_test))
 
 		for k, model in enumerate(model_names):
-				# deprecation warning
-				err_mod[k].append(fight(float(A_Y_test), float(B_Y_test), OBJ_models[k], OBJ_models_inv[k], A_models[k], A_models_inv[k], B_models[k], B_models_inv[k]))
+				d_A, d_B = fight(float(A_Y_test), float(B_Y_test), OBJ_models[k], OBJ_models_inv[k], A_models[k], A_models_inv[k], B_models[k], B_models_inv[k])
+				d_As.append(d_A)
+				d_Bs.append(d_B)
 
-for k, model in enumerate(model_names):
-	print(model+" mse:", mse([0 for i in err_mod[k]], err_mod[k]))
-print("zero mse:", mse([0 for i in err], err))
+# for k, model in enumerate(model_names):
+# 	print(model+" mse:", mse([0 for i in err_mod[k]], err_mod[k]))
+# print("zero mse:", mse([0 for i in err], err))
+
+with open(path+'dAs_dBs.pkl', 'wb') as f:
+	pkl.dump([d_As, d_Bs], f)
+print("done")
